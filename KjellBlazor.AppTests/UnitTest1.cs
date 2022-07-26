e8bca8428dca4008a892ac15662d4cecc1306427
Kjell Pinter <KjellPinter@gmail.com>
2022-07-26 11:01:59 -0700

    Project is running

---
+++ KjellBlazor.AppTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace KjellBlazor.AppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- KjellBlazorDemo.App/Program.cs
+++ KjellBlazorDemo.App/Program.cs
@@ -1,4 +1,4 @@
-using KjellBlazorDemo;
+using KjellBlazorDemo.App;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
