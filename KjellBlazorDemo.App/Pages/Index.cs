d67059db80e98ab2b9e9f10d1691a4540f03d691
Kjell Pinter <KjellPinter@gmail.com>
2022-09-16 15:40:19 -0700

    Move @Code from Index.razor to Index.cs

--- KjellBlazorDemo.App/Pages/Index.cs
+++ KjellBlazorDemo.App/Pages/Index.cs
@@ -1,5 +1,7 @@
 ﻿using KjellBlazorDemo.App.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Timers;
 
 namespace KjellBlazorDemo.App.Pages
 {
@@ -9,10 +11,46 @@ namespace KjellBlazorDemo.App.Pages
 
         protected SettingsDialog SettingsDialog { get; set; }
 
+        private ElementReference mainDiv;
+        private System.Timers.Timer? _timer;
 
         public Index()
         {
             SettingsDialog = new SettingsDialog();
+        }
+
+        private void HandleKeyDown(KeyboardEventArgs a)
+        {
+            Controls.KeyDown(a.Code);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool first)
+        {
+            if (first)
+            {
+                await mainDiv.FocusAsync();
+            }
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            _timer = new System.Timers.Timer();
+            _timer.Interval = 20;
+            _timer.Elapsed += TimerElapsed;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+
+            return base.OnInitializedAsync();
+        }
+
+        private void TimerElapsed(Object? source, ElapsedEventArgs a)
+        {
+            Redraw();
+        }
+
+        private void Redraw()
+        {
+            this.StateHasChanged();
         }
 
         public void ShowSettings()
