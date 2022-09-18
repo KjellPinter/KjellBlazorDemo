b203c1870fd7319d9d33c5d716e674d8c71226c2
Kjell Pinter <KjellPinter@gmail.com>
2022-09-18 12:56:17 -0700

    Randomized trash sprite, but in a poor way which will require refactoring but is ok for now.

--- KjellBlazorDemo.App/Models/Trash.cs
+++ KjellBlazorDemo.App/Models/Trash.cs
@@ -5,11 +5,31 @@
 
         public int Top { get; set; }
         public int Left { get; set; }
+        public int offsetY { get; set; }
+        public int offsetX { get; set; }
 
         internal Trash(int top, int left)
         {
             Top = top;
             Left = left;
+
+            offsetY = -64;
+
+            var rnd = new Random();
+            var roll = rnd.Next(3);
+
+            switch (roll)
+            {
+                case 0:
+                    offsetX = 0;
+                    break;
+                case 1:
+                    offsetX = -32;
+                    break;
+                default:
+                    offsetX = -64;
+                    break;
+            }
         }
 
     }
